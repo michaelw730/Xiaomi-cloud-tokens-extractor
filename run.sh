d06e5805b12ec22813c01d38231cc2daa0851a57
Piotr Machowski <PiotrMachowski@users.noreply.github.com>
2025-07-31 01:50:03 +0200

    Add detection of HA terminal in run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
+ha_host=$(ha network info --raw-json 2>/dev/null | jq ".data.interfaces[0].ipv4.address[0]" 2>/dev/null | sed "s/\/.*//g" 2>/dev/null | sed "s/\"//g" 2>/dev/null)
+if [ -z "${ha_host}" ]; then
+  ha_host="127.0.0.1"
+fi
+host_to_pass=$ha_host || "127.0.0.1"
 set -o errexit  # fail on first error
 set -o nounset  # fail on undef var
 set -o pipefail # fail on first error in pipe
@@ -8,6 +13,6 @@ curl --silent --fail --show-error --location --remote-name --remote-header-name\
 unzip token_extractor.zip
 cd token_extractor
 pip3 install -r requirements.txt
-python3 token_extractor.py
+python3 token_extractor.py --host $host_to_pass
 cd ..
 rm -rf token_extractor token_extractor.zip
